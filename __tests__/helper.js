c006cb7e91c2d2ca706cd07146df2286502b7110
Victor Johnson <johnsonsirv@gmail.com>
2023-03-22 22:41:53 +0100

    add test framework

---
+++ __tests__/helper.js
@@ -0,0 +1,23 @@
+const awilix = require('awilix');
+
+const bootstrap = require('../src/bootstrap');
+const container = require('../src/bootstrap/container');
+const serverModules = require('../src/server');
+const { loggerBootstrapMock } = require('./mocks/bootstrap/logger');
+
+const start = async (mckContainer) => {
+  mckContainer.register('logger', awilix.asValue(loggerBootstrapMock));
+
+  const { results } = await bootstrap(mckContainer, { serverModules });
+  return results;
+};
+
+const register = async (mckContainer, name, value) => (
+  mckContainer.register(name, awilix.asValue(value))
+);
+
+module.exports = {
+  start,
+  register,
+  container,
+};
